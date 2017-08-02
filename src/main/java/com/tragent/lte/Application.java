665203a277a890d2fca8c9a62005cc31c21d5105
Awasum Yannick <awasum.yannick@skylabase.com>
2017-08-02 09:50:32 +0100

    CORS Attempt from Bauldung

--- src/main/java/com/tragent/lte/Application.java
+++ src/main/java/com/tragent/lte/Application.java
@@ -3,11 +3,7 @@ package com.tragent.lte;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
 @EnableConfigurationProperties
@@ -17,28 +13,5 @@ public class Application /*implements CommandLineRunner */ {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	//@Resource
-	//StorageService storageService;
-
-	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
-				//registry.addMapping("auth").allowedMethods("RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS");
-			}
-		};
-	}
-	
-	/*
-	@Override
-	public void run(String... args) throws Exception {
-		storageService.deleteAll();
-		storageService.init();
-	}
-	*/
 
 }
